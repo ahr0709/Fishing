ffbd5dfc3bf1c06f88f42a32a66cbabb97e5fcec
John Ro <johnro54@yahoo.com>
2021-05-09 20:51:47 -0700

    new update/idea

--- myscripts.js
+++ myscripts.js
@@ -1,9 +1,19 @@
-$('.carousel').carousel({
-  interval: false
-});
+const layer1 = document.getElementById("boat");
+const layer2 = document.querySelector("fish");
+const bg = document.getElementById("bg");
+const moon = document.getElementById("moon");
+const mountain = document.getElementById("mountain");
+const road = document.getElementById("road");
+const text = document.getElementById("slideshow-text");
+const secondText = document.getElementById("secondText");
 
-function myFunction() {
-  let element = document.body;
-  element.classList.toggle("dark-mode");
-  }
-  
+// Our slideshow
+window.addEventListener("scroll", function () {
+  let value = window.scrollY;
+
+  bg.style.top = value * 0.5 + "px";
+  moon.style.left = -value * 0.5 + "px";
+  mountain.style.top = -value * 0.15 + "px";
+  road.style.top = value * 0.15 + "px";
+  text.style.top = value * 1.5 + "px";
+});
