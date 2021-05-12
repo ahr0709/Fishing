ecbbeb979044bb644661a41b8e0594518da71304
John Ro <johnro54@yahoo.com>
2021-05-11 19:53:05 -0700

    fix

--- myscripts.js
+++ myscripts.js
@@ -15,5 +15,6 @@ window.addEventListener("scroll", function () {
   moon.style.left = -value * 0.5 + "px";
   mountain.style.top = -value * 0.15 + "px";
   road.style.top = value * 0.15 + "px";
-  text.style.top = value * 1.5 + "px";
+  text.style.top = value * 1.2 + "px";
+  
 });
